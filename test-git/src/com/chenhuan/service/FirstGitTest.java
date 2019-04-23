f057a984e6c4b4cf6ae88ec182e958562aabe6c9
Frank Chen <Frank.Chen@nike.com>
2019-04-23 16:08:47 +0800

    第二次修改

--- test-git/src/com/chenhuan/service/FirstGitTest.java
+++ test-git/src/com/chenhuan/service/FirstGitTest.java
@@ -3,5 +3,7 @@ package com.chenhuan.service;
 public class FirstGitTest {
 	public static void main(String[] args) {
 		System.out.println("test commit for git");
+		
+		System.out.println("woshisui");
 	}
 }
