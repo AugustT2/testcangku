06a53b0debf92b3f8a885c6317b261e7b33f761d
Frank Chen <Frank.Chen@nike.com>
2019-04-25 14:41:26 +0800

    继续第二部分的开发

--- chuan-lucene/src/main/java/com/chuan/lucene/CreateTagTest.java
+++ chuan-lucene/src/main/java/com/chuan/lucene/CreateTagTest.java
@@ -4,5 +4,10 @@ public class CreateTagTest {
 	public static void main(String[] args) {
 		System.out.println("���create tag ��ɺ�����һ����֧��ӵ��ļ�");
 		System.out.println("上面那就乱码了啊");
+		
+		
+		
+		System.out.println("-----------------------");
+		System.out.println("继续第二部分的开发");
 	}
 }
