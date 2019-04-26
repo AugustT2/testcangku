822d64640959fcd79d74117a69efecb4abc401ad
Frank Chen <Frank.Chen@nike.com>
2019-04-26 16:49:40 +0800

    好无奈，上一个分支合并后，由于我在master上更改了，分支用不了。除非我吧master合并到branch-2上，但是我不想这么干，就新开了一个分支

--- chuan-lucene/src/main/java/com/chuan/lucene/CreateTagTest.java
+++ chuan-lucene/src/main/java/com/chuan/lucene/CreateTagTest.java
@@ -11,5 +11,7 @@ public class CreateTagTest {
 		System.out.println("继续第二部分的开发");
 		
 		System.out.println("di san kai shi le ha ");
+		
+		System.out.println("好无奈，上一个分支合并后，由于我在master上更改了，分支用不了。除非我吧master合并到branch-2上，但是我不想这么干，就新开了一个分支");
 	}
 }
